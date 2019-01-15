f199796a0daa4a1d67c100d7e60251b2743c9b22
Joe Scheiman <scheiman.joe@gmail.com>
2019-01-15 11:30:46 -0500

    Initial commit

---
+++ HelloWorldJoe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldJoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ HelloWorldJoeTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace HelloWorldJoeTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal("Hello", "Hello");
+        }
+    }
+}
